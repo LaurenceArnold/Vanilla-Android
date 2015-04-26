8ef8200a4d5b046823f9efbb23b155a6ec048c9a
LaurenceArnold <l.h.a.arnold@student.rug.nl>
2015-04-26 20:36:27 +0200

    Finished exercise 2F and 2G

--- op2.py
+++ op2.py
@@ -11,8 +11,8 @@ print("There are" + " " + str(len(words)) + " " + "words")
 print("There are" + " " + str(len(brown.tagged_sents(categories='mystery'))) + " " + "sentences")
 
 # Exercise 2B
-print(brown.tagged_words(categories='mystery')[99])
-print(brown.tagged_words(categories='mystery')[100])
+print(words[99])
+print(words[100])
 
 # Exercise 2C
 listOfTags = []
@@ -28,3 +28,16 @@ print("The top 10 words and their counts are: ", listOfWords.most_common(10))
 listWords = [word[1] for word in words]
 listOfWords = nltk.FreqDist(listWords)
 print("The top 10 POS tags and their counts are: ", listOfWords.most_common(10))
+
+# Exercise 2F
+# dus [0] is het woord, [1] is de POS-tag die wordt meegegeven
+adverbs = [word[0] for word in words if word[1] == 'RB']
+result = nltk.FreqDist(adverbs)
+print("There are ", str(len(result)), "words with the RB-tag")
+print("The most common are: ", result.most_common(1))
+
+#Exercise 2G:
+adjectiv = [word[0] for word in words if word[1] == 'JJ']
+result2 = nltk.FreqDist(adjectiv)
+print("There are: ", str(len(result2)), "words with the JJ-tag")
+print("The most common are ", result2.most_common(1))
