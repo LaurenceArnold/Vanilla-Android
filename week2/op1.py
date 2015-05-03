00ad3e8f67cf76ac2cee057d21e33322fadd16fb
LaurenceArnold <l.h.a.arnold@student.rug.nl>
2015-05-03 12:56:04 +0200

    updates

--- week2/op1.py
+++ week2/op1.py
@@ -2,20 +2,23 @@
 import nltk
 from nltk.collocations import *
 from nltk.util import ngrams
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import wordnet
 
 text = open('ada_lovelace.txt').read()
 source = text.decode("utf-8")
 sents = nltk.sent_tokenize(source)
-#lemmatizer = WordNetLemmatizer()
-# lemma definieren
+lemmatizer = WordNetLemmatizer()
 
 tokens = []
 for sent in sents:
     tokens += nltk.word_tokenize(sent)
 pos_tags = nltk.pos_tag(tokens)
 verbs = [word[0] for word in pos_tags if word[1] == 'VB' or word[1] == 'VBD' or word[1] == 'VBG' or word[1] == 'VBN' or word[1] =='VBP' or word[1] =='VBZ']
-print(verbs)
 
-    # lemmatizer.lemmatize(lemma, wordnet.VERB)
+result1 = []
+for word in verbs:
+    result = lemmatizer.lemmatize(word, wordnet.VERB )
+    result1.append(result)
 
-    #print verb_lemmas
+print(result1)
