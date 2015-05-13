f81e007e778f77c152fefc68fc31beb6b97ffe72
jarikoosting <oostingjarik@gmail.com>
2015-05-13 11:50:21 +0200

    Open files

--- week3/op1.py
+++ week3/op1.py
@@ -10,9 +10,17 @@ def main():
 
     # Loop through maps
     for root, dirs, filenames in os.walk(directory):
+
+        #
         for f in filenames:
+
+            # If file is the token file
             if f == "en.tok.off":
-                print(f)
+                with open(root+'/'+f, 'r') as in_f:
+                    for line in in_f:
+                        print(line)
+
+
 
 if __name__ == "__main__":
     main()
