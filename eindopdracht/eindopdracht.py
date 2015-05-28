091af3bd55828054619e936e0b97119f593f096d
jarikoosting <oostingjarik@gmail.com>
2015-05-28 17:20:34 +0200

    Improvement of code and fixed some errors

--- eindopdracht/eindopdracht.py
+++ eindopdracht/eindopdracht.py
@@ -9,26 +9,31 @@ def main():
     # Get the directory of the file
     directory = os.getcwd()
 
+    # List for the nouns
+    # Need to be outside of the loop because of the list comprehesion
+    NounList = []
+
     # Loop through maps
     for root, dirs, filenames in os.walk(directory):
-        # Every f is a file
+
+        # Loop trough files
         for file in filenames:
+
             # Check if the file is en.tok.off
-            if file == "en.tok.off":
+            if file == "en.tok.off.pos.ent":
+
                 # Open file
                 with open(root+'/'+file, 'r') as in_f:
-                # Loop through lines of file
+
+                    # Loop through lines of file
                     for line in in_f:
 
-                        #filter all nouns
-                        NounList = []
                         # Get tokens and append to list
-
                         columns = line.split()
                         token = columns[4]
                         NounList.append(token)
 
-    nouns = [(word) for word in NounList if tag.startswith("N")]
+    nouns = [tag for tag in NounList if tag.startswith("N")]
 
     # eerst standaard entity tagger laten runnen op de inputfiles
     # daarna de rest handmatig laten taggen via Wordnet & hypernyms
