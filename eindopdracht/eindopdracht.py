7174d4445d3c2e8878e58a914c27e970e3753d7b
jarikoosting <oostingjarik@gmail.com>
2015-05-28 17:25:02 +0200

    Added the NERTagger

--- eindopdracht/eindopdracht.py
+++ eindopdracht/eindopdracht.py
@@ -3,15 +3,18 @@
 import nltk
 from nltk.corpus import wordnet
 from nltk.wsd import lesk
+from nltk.tag.stanford import NERTagger
 
 
 def main():
     # Get the directory of the file
     directory = os.getcwd()
 
+    # All declaretions for variables here
     # List for the nouns
     # Need to be outside of the loop because of the list comprehesion
-    NounList = []
+    nounList = []
+    taggedNouns
 
     # Loop through maps
     for root, dirs, filenames in os.walk(directory):
@@ -31,14 +34,22 @@ def main():
                         # Get tokens and append to list
                         columns = line.split()
                         token = columns[4]
-                        NounList.append(token)
+                        nounList.append(token)
 
-    nouns = [tag for tag in NounList if tag.startswith("N")]
+    nouns = [tag for tag in nounList if tag.startswith("N")]
+
+    # MUC NER tagger
+    muc = NERTagger('/Library/Python/2.7/site-packages/stanford-ner-2014-06-16/classifiers/english.muc.7class.distsim.crf.ser.gz',
+                   '/Library/Python/2.7/site-packages/stanford-ner-2014-06-16/stanford-ner-3.4.jar')
+
+    # Tag all the nouns
+    muc.tag(nouns)
+
+
 
     # eerst standaard entity tagger laten runnen op de inputfiles
     # daarna de rest handmatig laten taggen via Wordnet & hypernyms
 
-    #dus jarik moet de eerste stap moeten we de de named entity laten runnen.
 
 
 
